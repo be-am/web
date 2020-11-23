de2349b856bc77f9a30391244bbda8a5d1050704
kingbobin <bo0433@naver.com>
2020-11-24 01:53:19 +0900

    register.php

---
+++ register.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html >
+<head></head>
+<body>
+    <form action="register_ok.php" method="post">
+        아이디:<input type="text" name="userID" />
+		비밀번호:<input type="password" name="userPassword" />
+		이름:<input type="text" name="userName" />
+        <button type="submit">제출</button>
+    </form>
+</body>
+</html>
