41580673690a3c7b0e03de8af9b9287c0063fc41
kingbobin <bo0433@naver.com>
2020-11-24 04:12:26 +0900

    login.php

---
+++ login.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head></head>
+<body>
+    <form action="login_ok.php" method="post">
+        아이디:<input type="text" name="userID"/>
+        비밀번호:<input type="text" name="userPassword"/>
+        <button type="submit">제출</button>
+    </form>
+</body>
+</html>
