ddeb29329db651d2fab200eb97ef89bed8a64c72
kingbobin <bo0433@naver.com>
2020-11-24 04:06:07 +0900

    change index

--- index.php
+++ index.php
@@ -3,6 +3,17 @@
 	<head></head>
 	<body>
 		<a href="register.php">회원가입</a>
+		<a href="login.php">로그인</a>
+
+		<?php 
+			session_start();
+			if($_SESSION['isLogged']){
+				echo $_SESSION['userID']."님, 환영합니다";
+			}
+			else{
+				echo "로그인을 해주세요";
+			}
+		?>
 	</body>
 </html>
 
